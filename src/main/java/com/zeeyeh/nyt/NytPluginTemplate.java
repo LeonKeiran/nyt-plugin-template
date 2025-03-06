517e1fc7c4486b18420da9821ab05f5503ce4ca3
LeonKeiran <bmlingqi@163.com>
2025-03-06 20:23:20 +0800

    插件主类

--- src/main/java/com/zeeyeh/nyt/NytPluginTemplate.java
+++ src/main/java/com/zeeyeh/nyt/NytPluginTemplate.java
@@ -1,17 +1,24 @@
 package com.zeeyeh.nyt;
 
+import com.zeeyeh.nyt.api.Translator;
+import com.zeeyeh.nyt.config.LanguageManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NytPluginTemplate extends JavaPlugin {
+    private LanguageManager languageManager;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        languageManager = new LanguageManager(this);
+        getLanguageManager().initializeDefaultLanguage();
+        Translator.init(languageManager);
+    }
 
+    public LanguageManager getLanguageManager() {
+        return languageManager;
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
