bf5b0b784cb6309f3699c9fc580235af167db7f6
LeonKeiran <bmlingqi@163.com>
2025-03-07 22:05:07 +0800

    插件主类

--- src/main/java/com/zeeyeh/nyt/NytPluginTemplate.java
+++ src/main/java/com/zeeyeh/nyt/NytPluginTemplate.java
@@ -2,20 +2,21 @@ package com.zeeyeh.nyt;
 
 import com.zeeyeh.nyt.api.Translator;
 import com.zeeyeh.nyt.config.LanguageManager;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NytPluginTemplate extends JavaPlugin {
+    @Getter
+    private static NytPluginTemplate instance;
+    @Getter
     private LanguageManager languageManager;
 
     @Override
     public void onEnable() {
+        instance = this;
         languageManager = new LanguageManager(this);
         getLanguageManager().initializeDefaultLanguage();
         Translator.init(languageManager);
-    }
-
-    public LanguageManager getLanguageManager() {
-        return languageManager;
     }
 
     @Override
